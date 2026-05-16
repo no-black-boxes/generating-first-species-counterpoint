755dd180c61737537194b87123d0854355372406
Aidan Bensch <aidanbensch@gmail.com>
2026-05-16 14:35:12 -0400

    Add simple CLI

--- src/harmonize/cli.py
+++ src/harmonize/cli.py
@@ -1,2 +1,22 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def _parse_args() -> tuple[Path, Path]:
+    parser = ArgumentParser(
+        prog="harmonize",
+        description="Generates four-part harmony using constraints",
+    )
+
+    parser.add_argument("input", help="JSON file with initial conditions")
+    parser.add_argument("output", help="JSON file for the solution")
+
+    args = parser.parse_args()
+
+    return Path(args.input), Path(args.output)
+
+
 def main() -> None:
-    print("Hello!")
+    in_path, out_path = _parse_args()
+    print(f"Input path: {in_path}")
+    print(f"Output path: {out_path}")
