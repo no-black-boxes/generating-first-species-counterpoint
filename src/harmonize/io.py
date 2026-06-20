4cb2e0ac6f87327d38c67335188c62434da281b2
Aidan Bensch <aidanbensch@gmail.com>
2026-06-20 15:59:25 -0400

    Update Solution to be a melody and counter-melody

--- src/harmonize/io.py
+++ src/harmonize/io.py
@@ -1,13 +1,6 @@
-from enum import Enum, auto
 import re
 
 from harmonize import solution as sol
-
-
-class Quality(Enum):
-    MAJOR = auto()
-    MINOR = auto()
-    DIMINISHED = auto()
 
 
 _PITCH_CLASS_NAMES = [
@@ -25,142 +18,34 @@ _PITCH_CLASS_NAMES = [
     "B",
 ]
 
-
-# We only need these seven, so I haven't written a roman numeral parser.
-_ROMAN_NUMERALS = ["i", "ii", "iii", "iv", "v", "vi", "vii"]
-
-
-_INVERSION_SYMBOLS = {
-    "": (False, 0),
-    "6": (False, 1),
-    "64": (False, 2),
-    "7": (True, 0),
-    "65": (True, 1),
-    "43": (True, 2),
-    "42": (True, 3),
-}
+_UNKNOWN_NAME = "X"
 
 
 def load_solution(data: dict) -> sol.Solution:
     """Loads a solution from JSON data."""
 
-    if "key" not in data or "notes" not in data or "chords" not in data:
-        raise ValueError("Solution must have 'key', 'notes', and 'chords' keys")
+    melody = _parse_voice(data["melody"])
+    counter_melody = _parse_voice(data["counterMelody"])
 
-    key = _parse_key(data["key"])
-    voices = _parse_voices(data["notes"])
-    chords = list(map(lambda chord: _parse_chord(key, chord), data["chords"]))
-
-    return sol.Solution(key, voices, chords)
+    return sol.Solution(melody, counter_melody)
 
 
-def _parse_key(key_data: dict) -> sol.Key:
-    if "tonic" not in key_data or "type" not in key_data:
-        raise ValueError("Key must have 'tonic' and 'type' keys")
+def _parse_voice(voice: list[str]) -> list[int]:
+    notes = []
 
-    match key_data["type"]:
-        case "major":
-            intervals = sol.Intervals([2, 2, 1, 2, 2, 2])
-        case "minor":
-            intervals = sol.Intervals([2, 1, 2, 2, 1, 2])
-        case _:
-            raise ValueError(f"'{key_data['type']}' is not a supported key")
+    for note_str in voice:
+        if note_str == _UNKNOWN_NAME:
+            notes.append(sol.UNKNOWN_NOTE)
+            continue
 
-    pitch_cls = sol.PitchCls(_pitch_class_int_from_str(key_data["tonic"]))
+        note_match = re.match(r"([A-G]#/[A-G]b|[A-G])(\d)", note_str)
 
-    return sol.Key(
-        pitch_cls,
-        intervals,
-    )
+        if note_match is None:
+            raise ValueError(f"{note_str} is not a valid note")
 
+        pitch_class = _PITCH_CLASS_NAMES.index(note_match[1])
+        octave = int(note_match[2])
 
-def _pitch_class_int_from_str(str_cls: str) -> int:
-    try:
-        return _PITCH_CLASS_NAMES.index(str_cls)
-    except ValueError:
-        raise ValueError(f"Pitch class {str_cls} not supported")
+        notes.append(octave * 12 + pitch_class + 12)
 
-
-def _parse_voices(voices_data: list[list]) -> dict[sol.Voice, list]:
-    voices = {}
-
-    if not len(voices_data) == 4:
-        raise ValueError("Solution must have four voices")
-
-    for index, notes in enumerate(voices_data):
-        voices[sol.Voice(index)] = list(map(_parse_note, notes))
-
-    return voices
-
-
-def _parse_note(note: str) -> sol.Note:
-    re_match = re.match(r"^[A-Z]#/[A-Z]b|^[A-Z]", note)
-
-    if re_match is None:
-        raise ValueError(f"{note} is not a valid note")
-
-    pitch_cls_int = _pitch_class_int_from_str(re_match[0])
-    octave = int(note[len(re_match[0]) :])
-
-    # 12 is C0.
-    return sol.Note(pitch_cls_int + octave * 12 + 12)
-
-
-def _parse_chord(key: sol.Key, chord: str) -> sol.Chord:
-    re_match = re.match(r"([IViv]+)(M|_dim|_hdim|)(\d*)", chord)
-
-    numeral = re_match[1]
-    quality_modifier = re_match[2]
-    inversion_symbol = re_match[3]
-
-    # Note this starts with "i" as 0.
-    try:
-        numeral_value = _ROMAN_NUMERALS.index(numeral.lower())
-    except ValueError:
-        raise ValueError(f"{numeral} is not a valid numeral")
-
-    root = key.get_scale_degree(numeral_value)
-
-    try:
-        is_seventh, inversion = _INVERSION_SYMBOLS[inversion_symbol]
-    except KeyError:
-        raise ValueError(f"{inversion_symbol} is not a valid inversion")
-
-    intervals = _parse_chord_intervals(numeral, quality_modifier, is_seventh)
-
-    return sol.Chord(root, intervals, inversion)
-
-
-def _parse_chord_intervals(
-    numeral: str, quality_modifier: str, is_seventh: bool
-) -> sol.Intervals:
-    if "dim" in quality_modifier:
-        intervals = [3, 3]
-    elif numeral == numeral.lower():
-        intervals = [3, 4]
-    elif numeral == numeral.upper():
-        intervals = [4, 3]
-    else:
-        raise ValueError(
-            f"{numeral}{quality_modifier} represents in invalid chord quality"
-        )
-
-    if not is_seventh:
-        if quality_modifier == "_hdim" or quality_modifier == "M":
-            raise ValueError(
-                f"{quality_modifier} suggests a seventh, but no inversion is provided"
-            )
-
-        return sol.Intervals(intervals)
-
-    if quality_modifier == "_dim":
-        # Diminished seventh
-        intervals.append(9 - sum(intervals))
-    elif quality_modifier == "_hdim" or len(quality_modifier) == 0:
-        # Minor seventh
-        intervals.append(10 - sum(intervals))
-    elif quality_modifier == "M":
-        # Major seventh
-        intervals.append(11 - sum(intervals))
-
-    return sol.Intervals(intervals)
+    return notes
--- tests/io_test.py
+++ tests/io_test.py
@@ -4,280 +4,76 @@ from harmonize import io
 from harmonize import solution as sol
 
 
-@pytest.fixture
-def dict_solution() -> dict:
-    return {
-        "key": {
-            "tonic": "C",
-            "type": "major",
-        },
-        "notes": [[], [], [], []],
-        "chords": [],
+def test_loads_midi_pitch_from_note_name():
+    sol_input = {
+        "melody": ["C4"],
+        "counterMelody": ["G#/Ab5"],
     }
 
+    expected_melody = [60]
+    expected_counter_melody = [80]
 
-@pytest.fixture
-def solution() -> sol.Solution:
-    return sol.Solution(
-        key=sol.Key(sol.PitchCls(0), sol.Intervals([2, 2, 1, 2, 2, 2])),
-        voices={
-            sol.Voice.SOPRANO: [],
-            sol.Voice.ALTO: [],
-            sol.Voice.TENOR: [],
-            sol.Voice.BASS: [],
-        },
-        chords=[],
-    )
+    solution = io.load_solution(sol_input)
+
+    assert expected_melody == solution.melody
+    assert expected_counter_melody == solution.counter_melody
 
 
-def test_reads_major_key(dict_solution, solution) -> None:
-    dict_solution["key"] = {
-        "tonic": "C",
-        "type": "major",
+def test_loads_unknown_pitch():
+    sol_input = {
+        "melody": ["C4"],
+        "counterMelody": ["X"],
     }
 
-    solution.key = sol.Key(sol.PitchCls(0), sol.Intervals([2, 2, 1, 2, 2, 2]))
+    expected_melody = [60]
+    expected_counter_melody = [sol.UNKNOWN_NOTE]
 
-    parsed = io.load_solution(dict_solution)
+    solution = io.load_solution(sol_input)
 
-    assert solution == parsed
+    assert expected_melody == solution.melody
+    assert expected_counter_melody == solution.counter_melody
 
 
-def test_reads_minor_key(dict_solution, solution) -> None:
-    dict_solution["key"] = {
-        "tonic": "F#/Gb",
-        "type": "minor",
+def test_loads_multiple_pitches():
+    sol_input = {
+        "melody": ["C4", "G4", "F4", "C5"],
+        "counterMelody": ["C3", "B2", "G3", "C4"],
     }
 
-    solution.key = sol.Key(sol.PitchCls(6), sol.Intervals([2, 1, 2, 2, 1, 2]))
+    expected_melody = [60, 67, 65, 72]
+    expected_counter_melody = [48, 47, 55, 60]
+    
+    solution = io.load_solution(sol_input)
 
-    parsed = io.load_solution(dict_solution)
+    assert expected_melody == solution.melody
+    assert expected_counter_melody == solution.counter_melody
 
-    assert solution == parsed
 
-
-def test_errors_on_invalid_pitch_class_in_key(dict_solution) -> None:
-    dict_solution["key"] = {
-        # Yes, it's enharmonically the same as C, but it's not supported.
-        "tonic": "B#",
-        "type": "minor",
+def test_error_on_invalid_pitch():
+    sol_input = {
+        "melody": ["H5"],
+        "counterMelody": ["C3"],
     }
-
+    
     with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
+        io.load_solution(sol_input)
 
 
-def test_errors_on_invalid_key_type(dict_solution) -> None:
-    dict_solution["key"] = {
-        "tonic": "C",
-        "type": "Major",
+def test_error_on_length_difference():
+    sol_input = {
+        "melody": ["C4", "D4"],
+        "counterMelody": ["C3"],
     }
-
+    
     with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
+        io.load_solution(sol_input)
 
 
-def test_reads_one_note(dict_solution, solution) -> None:
-    dict_solution["notes"][sol.Voice.SOPRANO.value] = ["C4"]
-
-    solution.voices[sol.Voice.SOPRANO] = [sol.Note(60)]
-
-    parsed = io.load_solution(dict_solution)
-
-    assert solution == parsed
-
-
-def test_reads_multiple_notes(dict_solution, solution) -> None:
-    dict_solution["key"] = {
-        "tonic": "A#/Bb",
-        "type": "major",
+def test_error_on_unknown_in_melody():
+    sol_input = {
+        "melody": ["X"],
+        "counterMelody": ["C3"],
     }
-
-    dict_solution["notes"][sol.Voice.SOPRANO.value] = ["A#/Bb4", "G4", "F4", "F4"]
-    dict_solution["notes"][sol.Voice.ALTO.value] = ["F4", "D#/Eb4", "C4", "D4"]
-    dict_solution["notes"][sol.Voice.TENOR.value] = ["D4", "A#/Bb3", "A3", "A#/Bb3"]
-    dict_solution["notes"][sol.Voice.BASS.value] = ["A#/Bb2", "D#/Eb3", "F3", "A#/Bb2"]
-
-    solution.key = sol.Key(sol.PitchCls(10), sol.Intervals([2, 2, 1, 2, 2, 2]))
-
-    solution.voices[sol.Voice.SOPRANO] = [
-        sol.Note(70),
-        sol.Note(67),
-        sol.Note(65),
-        sol.Note(65),
-    ]
-    solution.voices[sol.Voice.ALTO] = [
-        sol.Note(65),
-        sol.Note(63),
-        sol.Note(60),
-        sol.Note(62),
-    ]
-    solution.voices[sol.Voice.TENOR] = [
-        sol.Note(62),
-        sol.Note(58),
-        sol.Note(57),
-        sol.Note(58),
-    ]
-    solution.voices[sol.Voice.BASS] = [
-        sol.Note(46),
-        sol.Note(51),
-        sol.Note(53),
-        sol.Note(46),
-    ]
-
-    parsed = io.load_solution(dict_solution)
-
-    assert solution == parsed
-
-
-def test_reads_negative_octave(dict_solution, solution) -> None:
-    dict_solution["notes"][sol.Voice.SOPRANO.value] = ["C-1"]
-
-    solution.voices[sol.Voice.SOPRANO] = [sol.Note(0)]
-
-    parsed = io.load_solution(dict_solution)
-
-    assert solution == parsed
-
-
-def test_errors_on_invalid_pitch_class(dict_solution) -> None:
-    dict_solution["notes"][sol.Voice.SOPRANO.value] = ["H4"]
-
+    
     with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_errors_on_non_alphabetic_pitch_class(dict_solution) -> None:
-    dict_solution["notes"][sol.Voice.SOPRANO.value] = ["@4"]
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-@pytest.mark.parametrize(
-    "chord_str, root, intervals, inversion",
-    [
-        ("I", sol.PitchCls(0), sol.Intervals([4, 3]), 0),
-        ("V", sol.PitchCls(7), sol.Intervals([4, 3]), 0),
-        ("vi", sol.PitchCls(9), sol.Intervals([3, 4]), 0),
-        ("vii_dim", sol.PitchCls(11), sol.Intervals([3, 3]), 0),
-        ("IV6", sol.PitchCls(5), sol.Intervals([4, 3]), 1),
-        ("vii_dim64", sol.PitchCls(11), sol.Intervals([3, 3]), 2),
-        ("V7", sol.PitchCls(7), sol.Intervals([4, 3, 3]), 0),
-        ("V65", sol.PitchCls(7), sol.Intervals([4, 3, 3]), 1),
-        ("V43", sol.PitchCls(7), sol.Intervals([4, 3, 3]), 2),
-        ("V42", sol.PitchCls(7), sol.Intervals([4, 3, 3]), 3),
-        ("IVM7", sol.PitchCls(5), sol.Intervals([4, 3, 4]), 0),
-        ("vi7", sol.PitchCls(9), sol.Intervals([3, 4, 3]), 0),
-        ("vii_hdim7", sol.PitchCls(11), sol.Intervals([3, 3, 4]), 0),
-        ("vii_dim7", sol.PitchCls(11), sol.Intervals([3, 3, 3]), 0),
-        ("vii_dim65", sol.PitchCls(11), sol.Intervals([3, 3, 3]), 1),
-    ],
-)
-def test_reads_single_chord(
-    dict_solution, solution, chord_str, root, intervals, inversion
-) -> None:
-    dict_solution["chords"] = [chord_str]
-
-    solution.chords = [sol.Chord(root, intervals, inversion)]
-
-    parsed = io.load_solution(dict_solution)
-
-    assert solution == parsed
-
-
-@pytest.mark.parametrize(
-    "chord_str",
-    [
-        "viiii",
-        "Vi",
-        "I5",
-        "vii_hdim",
-        "VM",
-    ],
-)
-def test_errors_on_invalid_single_chord(dict_solution, chord_str) -> None:
-    dict_solution["chords"] = [chord_str]
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_reads_single_chord_in_different_key(dict_solution, solution) -> None:
-    dict_solution["key"] = {
-        "tonic": "D",
-        "type": "minor",
-    }
-
-    dict_solution["chords"] = ["V"]
-
-    solution.key = sol.Key(sol.PitchCls(2), sol.Intervals([2, 1, 2, 2, 1, 2]))
-    solution.chords = [sol.Chord(sol.PitchCls(9), sol.Intervals([4, 3]), 0)]
-
-    parsed = io.load_solution(dict_solution)
-
-    assert solution == parsed
-
-
-def test_errors_when_missing_key() -> None:
-    dict_solution = {
-        "notes": [[], [], [], []],
-        "chords": [],
-    }
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_errors_when_missing_notes() -> None:
-    dict_solution = {
-        "key": {
-            "tonic": "C",
-            "type": "major",
-        },
-        "chords": [],
-    }
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_errors_when_missing_chords() -> None:
-    dict_solution = {
-        "key": {
-            "tonic": "C",
-            "type": "major",
-        },
-        "notes": [[], [], [], []],
-    }
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_errors_when_malformed_key() -> None:
-    dict_solution = {
-        "key": {
-            "root": "C",
-            "type": "major",
-        },
-        "notes": [[], [], [], []],
-        "chords": [],
-    }
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
-
-
-def test_errors_when_wrong_voice_count() -> None:
-    dict_solution = {
-        "key": {
-            "tonic": "C",
-            "type": "major",
-        },
-        "notes": [[], [], []],
-        "chords": [],
-    }
-
-    with pytest.raises(ValueError):
-        io.load_solution(dict_solution)
+        io.load_solution(sol_input)
