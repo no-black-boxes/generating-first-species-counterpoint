d4e6daf26b47e79c49e590a7823ff45e1b0b2f6d
Aidan Bensch <aidanbensch@gmail.com>
2026-06-27 19:22:12 -0400

    Select randomly from the key

--- src/harmonize/solver.py
+++ src/harmonize/solver.py
@@ -5,10 +5,15 @@ from harmonize import solution as sol
 
 class Solver:
     def solve(self, in_solution: sol.Solution) -> sol.Solution:
+        domain = []
+        for note in range(sol.MIN_NOTE, sol.MAX_NOTE):
+            if (note % 12) in in_solution.key:
+                domain.append(note)
+
         counter_melody = in_solution.counter_melody.copy()
 
         for i in range(len(counter_melody)):
             if counter_melody[i] == sol.UNKNOWN_NOTE:
-                counter_melody[i] = random.randrange(sol.MIN_NOTE, sol.MAX_NOTE)
+                counter_melody[i] = random.choice(domain)
 
         return sol.Solution(in_solution.melody, counter_melody, in_solution.key)
